f94b0db408fad554ff67c25eb203c8d50ddcaf6a
iometrine <iometrine@free.fr>
2021-03-19 12:02:47 +0100

    first commit

---
+++ src/app/providers/webrtc.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import Peer from 'peerjs';
+declare var cordova: any;
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WebrtcService {
+  peer: Peer;
+  myStream: MediaStream;
+  myEl: HTMLMediaElement;
+  partnerEl: HTMLMediaElement;
+
+  stun = 'stun.l.google.com:19302';
+  mediaConnection: Peer.MediaConnection;
+  options: Peer.PeerJSOption;
+  stunServer: RTCIceServer = {
+    urls: 'stun:' + this.stun,
+  };
+
+  constructor(  public platform: Platform  ) {
+    
+    this.options = {  // not used, by default it'll use peerjs server
+      key: 'cd1ft79ro8g833di',
+      debug: 3
+    };
+  }
+
+  getMedia() {
+    if (this.platform.is("ios")) {
+      if (cordova.plugins) {
+        if (cordova.plugins.iosrtc) {
+          // Expose WebRTC and GetUserMedia SHIM as Globals (Optional)
+          // Alternatively WebRTC API will be inside cordova.plugins.iosrtc namespace
+          cordova.plugins.iosrtc.registerGlobals();
+          // Enable iosrtc debug (Optional)
+          cordova.plugins.iosrtc.debug.enable('*', true);
+        }
+      }
+    }
+
+    navigator.getUserMedia({ audio: true, video: true }, (stream) => {
+      this.handleSuccess(stream);
+    }, (error) => {
+      this.handleError(error);
+    });
+  }
+
+  async init(userId: string, myEl: HTMLMediaElement, partnerEl: HTMLMediaElement) {
+    this.myEl = myEl;
+    this.partnerEl = partnerEl;
+    try {
+      this.getMedia();
+    } catch (e) {
+      this.handleError(e);
+    }
+    await this.createPeer(userId);
+  }
+
+  async createPeer(userId: string) {
+    this.peer = new Peer(userId);
+    this.peer.on('open', () => {
+      this.wait();
+    });
+  }
+
+  call(partnerId: string) {
+    const call = this.peer.call(partnerId, this.myStream);
+    call.on('stream', (stream) => {
+      this.partnerEl.srcObject = stream;
+    });
+  }
+
+  wait() {
+    this.peer.on('call', (call) => {
+      call.answer(this.myStream);
+      call.on('stream', (stream) => {
+        this.partnerEl.srcObject = stream;
+      });
+    });
+  }
+
+  handleSuccess(stream: MediaStream) {
+    this.myStream = stream;
+    this.myEl.srcObject = stream;
+  }
+
+  handleError(error: any) {
+    if (error.name === 'ConstraintNotSatisfiedError') {
+      
+      //const v = constraints.video;
+     // this.errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+      this.errorMsg(`The resolution px is not supported by your device.`);
+    } else if (error.name === 'PermissionDeniedError') {
+      this.errorMsg('Permissions have not been granted to use your camera and ' +
+        'microphone, you need to allow the page access to your devices in ' +
+        'order for the demo to work.');
+    }
+    this.errorMsg(`getUserMedia error: ${error.name}`, error);
+  }
+
+  errorMsg(msg: string, error?: any) {
+    const errorElement = document.querySelector('#errorMsg');
+    errorElement.innerHTML += `<p>${msg}</p>`;
+    if (typeof error !== 'undefined') {
+      console.error(error);
+    }
+  }
+}
